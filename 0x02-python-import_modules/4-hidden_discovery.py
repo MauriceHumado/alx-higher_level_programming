632b67acf3f09e8fad2ba94d534d28ac9ab25161
MauriceHumado <mauricehumado@gmail.com>
2023-01-12 09:13:10 +0000

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,8 @@ import sys
 import hidden_4 as hidden
 
 if __name__ != "__main__":
-	exit()
+    exit()
 
 for name in dir(hidden):
-	if name[0:2] != "__":
-		 print(name)
+    if name[0:2] != "__":
+        print(name)
